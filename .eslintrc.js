377e4fcf643b66dee50b0ddf485badbb418db64f
techocea <dunsfordbright@gmail.com>
2024-05-07 21:14:19 +0530

    changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
   },
   rules: {
-    // Additional rules or overrides
+    'prettier/prettier': 'error', // Enforce Prettier's rules
   },
 };
