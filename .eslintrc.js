7992b59079a5a1ae88550732a879e3ec94e69c83
techocea <dunsfordbright@gmail.com>
2024-05-07 20:49:22 +0530

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    // Additional rules or overrides
+  },
+};
